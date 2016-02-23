da0cb969f25e5ad9b0de1f3e3c2c2bdbe1306a5a
Alex Harman <alexanderjharman@gmail.com>
2016-02-23 01:30:37 +0000

    Splashscreen button registers clicks

--- src/js/start.js
+++ src/js/start.js
@@ -11,6 +11,19 @@ var startState = {
 
     create: function() {
         var bg = game.add.image(0,0, "splashScreen");
+        this.button = new Phaser.Rectangle(167, 493, 443, 117);
+        game.input.onDown.add(this.handleClick.bind(startState));
+    },
+
+    render: function() {
+        game.debug.geom(this.button,'#000000');
+    },
+
+    handleClick: function(pointer) {
+        if (this.button.contains(pointer.x, pointer.y)) { 
+            console.log('here'); 
+        } 
     }
 
-};
+}
+    
